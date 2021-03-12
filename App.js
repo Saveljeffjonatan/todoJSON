d972da869f55455469cb57e97c94026de854e9f9
Saveljeffjonatan <saveljeffjonatan@gmail.com>
2021-03-12 10:47:55 +0100

    WIP

--- App.js
+++ App.js
@@ -1,47 +1,121 @@
-const output = document.querySelector('#output');
-const addTodo = document.querySelector('#addTodo')
-const title = document.querySelector('#title')
-const todoText = document.querySelector('#todoText')
+const form = document.querySelector('#todoForm');
+const output = document.querySelector('#todoList');
+const inputBox = document.querySelector('.inputField input')
+const input = document.querySelector('#todoInput')
+const addBtn = document.querySelector('.inputField button')
+const deleteAllBtn = document.querySelector('.footer button')
 
+let todos = [];
 
-const pushTodo = () => {
-  if(title && todoText !== '')
+const fetchTodos = () => {
+  fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then(res => res.json())
+    .then(data => {
+      todos = data;
 
-  createTodo(title.value, todoText.value)
+      listTodos();
+      checkTodoStatus();
+    })
 }
+fetchTodos();
 
-const createTodo = (title, todoText) => {
-  let todo = {
-    title,
-    todoText
-  }
-
-  posts.push(todo)
-}
-
-//* Fetches my rest api and prints out a list of 10 todo posts.
-const listPosts = async () => {
-  let url = 'https://jsonplaceholder.typicode.com/posts?_limit=10'
-
-  const res = await fetch(url);
-  const posts = await res.json();
-
+const listTodos = () => {
   output.innerHTML = '';
-  posts.forEach(post => {
-    let html = `
-    <div class="card my-3">
-        <h3>${post.title.slice(0, 20)}</h3>
-        <p>${post.body.slice(0, 50)}</p>
+    todos.forEach(post => { //* Lists each array index as a separate post with different styling
+
+/*
+  Displays a fullsize box or small box depending on how many characters
+  there is + adds styling if the post is already completed or not.
+*/
+  if(post.completed === true){
+    let html =
+    `
+    <div id=${post.id} ${post.completed} class="post">
+    <li><h3 class="textEdit">${post.title.slice(0, 50)}</h3></li>
+    <button class="far fa-check-square"></button>
     </div>
     `
-
     output.innerHTML += html;
+  }else{
+    let html =
+    `
+    <div id=${post.id} ${post.completed} class="post">
+    <li><h3>${post.title.slice(0, 50)}</h3></li>
+    <button class="far fa-square"></button>
+    </div>
+    `
+    output.innerHTML += html;
+  }
 
+  const pendingNumb = document.querySelector('.pendingNumb')
+    pendingNumb.textContent = todos.length; //* Shows current pending tasks
   })
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-  listPosts();
+const createTodo = (title) => {
+  fetch('https://jsonplaceholder.typicode.com/todos', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    },
+    body: JSON.stringify({
+      title,
+      completed: false
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+
+    let newTodo = {
+      ...data,
+      id: Date.now().toString()
+    }
+    console.log(newTodo);
+    todos.unshift(newTodo);
+    listTodos();
+  })
+}
+
+inputBox.onkeyup = () => {
+  let userData = inputBox.value //* Getting user entered value
+  if(userData.trim() !== '') {
+    addBtn.classList.add('active') //* Will activate add button
+  } else {
+    addBtn.classList.remove('active') //* Will deactivate add button
+  }
+}
+
+output.addEventListener('click', e => {
+  const checkBox = e.target
+  const check = e.target.parentNode.querySelector('h3')
+
+  if(checkBox.classList.contains('fa-square')){
+    checkBox.classList.remove('fa-square')
+    checkBox.classList.add('fa-check-square')
+    check.classList.add('textEdit')
+  }else if(checkBox.classList.contains('fa-check-square')){
+    checkBox.classList.remove('fa-check-square')
+    check.classList.remove('textEdit')
+    checkBox.classList.add('fa-square')
+  }
 })
 
+const checkTodoStatus = () => {
+  const hasCompletedTrue = [...output.children].find(item => item.children[1].classList.contains('fa-check-square'))
+  if(hasCompletedTrue){
+    deleteAllBtn.classList.add('active')
+  }else {
+    deleteAllBtn.classList.remove('active')
+  }
+}
 
+addEventListener('click', () => checkTodoStatus())
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  createTodo(input.value);
+  form.reset();
+  addBtn.classList.remove('active')
+})
